16cf0d2ef5774c657aab616cef79f31a7a5ba860
Murilo Amaral <muriloamaciel@gmail.com>
2024-03-12 02:16:17 -0300

    feat: mostrando que lista com id diferentes

--- 01 - Estrutura de dados/01 - Listas/09_copy.py
+++ 01 - Estrutura de dados/01 - Listas/09_copy.py
@@ -1,5 +1,9 @@
 lista = [1, "Python", [40, 30, 20]]
 
-lista.copy()
+l2 = lista.copy()
 
 print(lista)  # [1, "Python", [40, 30, 20]]
+print(l2)  # [1, "Python", [40, 30, 20]]
+
+print(id(lista))
+print(id(l2))
